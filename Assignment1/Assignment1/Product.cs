a8296e39d90d42ac76096c774649b659253914d3
HishamR <ramishhisham@gmail.com>
2015-09-22 23:44:42 +0200

    Added Assignment1

---
+++ Assignment1/Assignment1/Product.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+        This class contains the object Product and its attributes.
+*/
+namespace Assignment1
+{
+    class Product
+    {
+
+        private string name;
+        private int count;
+        private double unitPrice;
+        private bool foodItem;
+
+        private double totalVAT, totalNetValue;
+
+        private const double foodVATrate = 0.12, otherVATRate = 0.25;
+
+        //Start-method 
+        public void Start()
+        {
+            //Read input
+            ReadInput();
+
+            //Calculate total tax
+            CalculateValues();
+
+            //Calculate totalNetPrice, total price
+            PrintReceipt();
+
+        }
+
+        //Method that reads the users input of Productinformation
+        private void ReadInput()
+        {
+            //1. Read name of the product
+            ReadName();
+
+            //2. Read price without VAT
+            ReadNetUnitPrice();
+
+            //3. Ask the user if the item is a food item
+            ReadIfFoodItem();
+
+            //4. Read number of items (quantity)
+            ReadCount();
+
+        }
+
+        //Reads the name of the product
+        private void ReadName()
+        {
+            Console.WriteLine();
+            Console.Write("Enter the product name:  ");
+
+            name = (Console.ReadLine());
+        }
+
+        //Reads the unitprice of the product
+        private void ReadNetUnitPrice()
+        {
+            Console.Write("Net unit price:  ");
+
+            unitPrice = double.Parse(Console.ReadLine());
+        }
+
+        //Reads if the product is a fooditem
+        private void ReadIfFoodItem()
+        {
+            Console.Write("Food item (y/n): ");
+
+            char response = char.Parse(Console.ReadLine());
+
+            if ((response == 'y') || (response == 'Y'))
+                foodItem = true;
+            else
+                foodItem = false;
+        }
+
+        //Reads the amount of product to calculate
+        private void ReadCount()
+        {
+            Console.Write("Count:  ");
+
+            count = int.Parse(Console.ReadLine());
+        }
+
+
+        //Calculate totalNetValue TotalVat
+        private void CalculateValues()
+        {
+            if (foodItem)
+            {
+                totalVAT = (unitPrice * foodVATrate) * count;
+                totalNetValue = (unitPrice * (1+foodVATrate)) * count;
+            }
+            else
+            {
+                totalVAT = (unitPrice * otherVATRate) * count;
+                totalNetValue = (unitPrice * (1 + otherVATRate)) * count;
+            }
+            
+
+        }
+
+        //Print results
+        private void PrintReceipt()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("****************" + "\t" +
+                              "WELCOME TO APU's SUPERMARKET \t" +
+                              "**************** ");
+
+            Console.WriteLine("*** ");
+
+            Console.WriteLine("*** " + 
+                              "Name of the product: \t\t\t" +
+                               name);
+
+            Console.WriteLine("*** " + 
+                              "Quantity: \t\t\t\t\t" +
+                              count);
+
+            Console.WriteLine("*** " +
+                              "Unit price: \t\t\t\t" +
+                               unitPrice);
+
+
+            Console.WriteLine("*** " + 
+                              "Food item: \t\t\t\t\t" +
+                              foodItem);
+
+            Console.WriteLine("*** ");
+
+            Console.WriteLine("*** " + 
+                              "Total amount to pay: \t\t\t" + 
+                               totalNetValue);
+
+            if (foodItem)
+            {
+                Console.WriteLine("*** " + 
+                                  "Including VAT at 12%: \t\t\t" + 
+                                   totalVAT);
+            }
+
+            else
+            {
+                Console.WriteLine("*** " +
+                                  "Including VAT at 25%: \t\t\t" +
+                                   totalVAT);
+            }
+
+            Console.WriteLine("*** ");
+
+            Console.WriteLine("*** " +
+                              "This program has been developed by: Hisham Ramish");
+
+            Console.WriteLine("**************** \t" +
+                              "PLEASE COME AGAIN \t\t" +
+                              "**************** ");
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ Assignment1/Assignment1VG/Table.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+        Author: Hisham Ramish
+        This class contains the object Table and its attributes.
+*/
+
+namespace Assignment1VG
+{
+    class Table
+    {
+        private int height = 60, width = 120;
+        private string color = "Black";
+        private double price = 199.99;
+        private bool run = true;
+        
+        //Start method 
+        public void Start()
+        {
+            while (run)
+            {
+                //Calculate totalNetPrice, total price
+                PrintInfo();
+
+                //Read input
+                ReadInput();
+
+            }
+
+        }
+
+        //Prints information about the table
+        private void PrintInfo()
+        {
+            Console.WriteLine("TABLE INFO");
+            Console.WriteLine();
+            Console.WriteLine("Color: \t\t\t" + color);
+            Console.WriteLine("Height: \t\t" + height);
+            Console.WriteLine("Width: \t\t\t" + width);
+            Console.WriteLine("Price: \t\t\t" + price);
+            Console.WriteLine();
+        }
+
+        //Outputs choices and determines user action
+        private void ReadInput()
+        {
+
+            Console.WriteLine("1. Change color");
+            Console.WriteLine("2. Change height");
+            Console.WriteLine("3. Change width");
+            Console.WriteLine("4. Change price");
+            Console.WriteLine("0. Exit");
+
+
+            Console.WriteLine();
+            Console.Write("Enter your choice:  ");
+
+            Action(int.Parse(Console.ReadLine()));
+
+        }
+
+        //User determines choice of method used
+        private void Action(int choice)
+        {
+            switch (choice)
+            {
+                case 0:
+                    run = false;
+                    break;
+                case 1:
+                    //1. Read name of the product
+                    ChangeColor();
+                    break;
+
+                case 2:
+                    //2. Read price without VAT
+                    ChangeHeight();
+                    break;
+
+                case 3:
+                    //3. Ask the user if the item is a food item
+                    ChangeWidth();
+                    break;
+
+                case 4:
+                    ChangePrice();
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice!");
+                    ReadInput();
+                    break;
+
+            }
+        }
+
+        //Changes price of table
+        private void ChangePrice()
+        {
+            Console.WriteLine();
+            Console.Write("Enter new price: ");
+
+            price = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Price changed!");
+            Console.WriteLine();
+        }
+
+        //Changes width of table
+        private void ChangeWidth()
+        {
+            Console.WriteLine();
+            Console.Write("Enter new width: ");
+
+            width = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Width changed!");
+            Console.WriteLine();
+        }
+
+        //Changes height of table
+        private void ChangeHeight()
+        {
+            Console.WriteLine();
+            Console.Write("Enter new height: ");
+
+            height = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Height changed!");
+            Console.WriteLine();
+        }
+
+        //Changes color of table
+        private void ChangeColor()
+        {
+            Console.WriteLine();
+            Console.Write("Enter new color: ");
+
+            color = Console.ReadLine();
+
+            Console.WriteLine("Color changed!");
+            Console.WriteLine();
+        }
+    
+    }
+
+
+}
